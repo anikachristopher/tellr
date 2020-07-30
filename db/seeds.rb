61e0dbb128a66e65ac1606fda7920186db9620a2
anikachristopher <anika.mchristoper@gmail.com>
2020-07-29 22:39:33 -0400

    create seed data for owners

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Owner.create(name: "Kendrick Lamar", username: "kendrickl", email: "kendl123@gmail.com", location: "Los Angeles, CA")
+Owner.create(name: "Nasir Jones", username: "nasj", email: "nas456@gmail.com", location: "Queens, NY")
+Owner.create(name: "Brian Bridges", username: "brianb", email: "bb234@gmail.com", location: "Atlanta, GA")
+Owner.create(name: "Melissa Elliot", username: "missye", email: "melliot@gmail.com", location: "Portsmouth, VA")
+Owner.create(name: "Lauryn Hill", username: "laurynh", email: "laurynh345@gmail.com", location: "East Orange, NJ")
+Owner.create(name: "Eve Cooper", username: "evec", email: "evecoop@gmail.com", location: "Philadelphia, PA")
+
+
+
